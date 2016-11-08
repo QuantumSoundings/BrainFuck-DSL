baaf44ef97266d2c5626456d49c24c5f94a2d175
Jordan <jshowe95@gmail.com>
2016-11-08 10:13:12 -0600

    Update KeywordObjects.scala

--- src/bfdsl/KeywordObjects.scala
+++ src/bfdsl/KeywordObjects.scala
@@ -1,21 +1,11 @@
-
 package bfdsl
 
-trait Tree[+StateWord]
-case class Leaf[StateWord](word: StateWord) extends Tree[StateWord]
-case class Branch[StateWord](left:Tree[StateWord],right:Tree[StateWord]) extends Tree[StateWord]
 
-object Tree {
-  def apply(word:StateWord) ={
-    
-    this   
-  }
-}
 abstract sealed class StateWord
 object R extends StateWord
 object L extends StateWord
 object A extends StateWord
-
+object END extends StateWord
 object M extends StateWord
 object P extends StateWord
 object I extends StateWord
@@ -24,8 +14,5 @@ abstract sealed class ControlWord
 object LS extends StateWord
 object LE extends StateWord
 object | extends StateWord
-object E extends StateWord
-
-case class tree(word:StateWord){
-  
-}
+object C extends StateWord
+object X extends StateWord
